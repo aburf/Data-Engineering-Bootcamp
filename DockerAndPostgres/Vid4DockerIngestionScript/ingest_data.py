a991b6713449d8ec737b6839e17467662112d230
Andrew <77085812+aburf@users.noreply.github.com>
2025-01-28 09:29:08 +0000

    save current progress on ingestion py ile and terminal command

---
+++ DockerAndPostgres/Vid4DockerIngestionScript/ingest_data.py
@@ -0,0 +1,59 @@
+
+import pandas as pd
+import argparse
+import os  
+##requires pip install sqlalchemy in codespaces
+from sqlalchemy import create_engine
+from time import time
+
+##user, password, host, port, database name,  table name
+##url of the csv
+
+def main(params):
+        user=params.user
+        password = params.password
+        host = params.host
+        port = params.port
+        db = params.db
+        table_name = params.tabe_name
+        data_url = '/workspaces/Data-Engineering-Bootcamp/yellow_tripdata_1000.csv'
+        csv_name = data_url.split('/')[-1]
+
+    os.system(f"wget {url} -O {csv_name}")
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df_iter = pd.read_csv(data_url,iterator=True, chunksize=100)
+    
+    df = next(df_iter)
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists = 'replace')
+    
+    while True:
+        t_start = time()
+        df = next(df_iter)
+
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+        df.to_sql(name=table_name, con=engine, if_exists = 'append')
+        t_end = time()
+        print('Inserted another chunk..., to %.3f seconds'% (t_end-t_start)) 
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+                        description='Ingest csv data to postgres',
+                        epilog='bottomo text')
+    parser.add_argument('user')
+    parser.add_argument('pass')
+    parser.add_argument('host')
+    parser.add_argument('port')
+    parser.add_argument('db')
+    parser.add_argument('table_name')
+    parser.add_argument('url')
+
+    args = parser.parse_args()
+
+    main(args)
--- DockerAndPostgres/Vid4DockerIngestionScript/pipeline.py
+++ DockerAndPostgres/Vid4DockerIngestionScript/pipeline.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
 
 
 import pandas as pd
 import os
+##requires pip install sqlalchemy in codespaces
+from sqlalchemy import create_engine
+from time import time
 
-
-# In[3]:
-
-
-pd.__version__
-os.getcwd()
-
-
-# In[4]:
+##need to pip install psycopg2-binary'
+engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+##engine.connect()
 
 
 data_url = '/workspaces/Data-Engineering-Bootcamp/yellow_tripdata_1000.csv'
@@ -24,96 +17,28 @@ df = pd.read_csv(data_url, nrows=50)
 df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
 df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-
-# In[5]:
-
-
-df.head()
-
-
-# In[7]:
-
-
-##requires pip install sqlalchemy in codespaces
-from sqlalchemy import create_engine
-
-
-# In[9]:
-
-
-##need to pip install psycopg2-binary
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-
-
-# In[10]:
-
-
-engine.connect()
-
-
-# In[11]:
-
-
-###get  an approach for creating this table in postgres
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data',con=engine))
-
-
-# In[12]:
-
-
 df_iter = pd.read_csv(data_url,iterator=True, chunksize=100)
 
 
-# In[13]:
-
-
 df = next(df_iter)
-len(df)
 df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
 df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-
-# In[14]:
-
-
-df.head(n=0)
-
-
-# In[15]:
-
 
 df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists = 'replace')
 
 
-# In[16]:
-
-
-get_ipython().run_line_magic('time', "df.to_sql(name='yellow_taxi_data', con=engine, if_exists = 'append')")
-
-
-# In[17]:
-
-
-from time import time
-
-
-# In[18]:
 
 
 ####iterate through batches. I ran low on memory but should work in a better VM
-
-for x in df_iter:
+while True:
+#for x in df_iter:
     t_start = time()
     df = next(df_iter)
+
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
     df.to_sql(name='yellow_taxi_data', con=engine, if_exists = 'append')
     t_end = time()
     print('Inserted another chunk..., to %.3f seconds'% (t_end-t_start)) 
-
-
-# In[ ]:
-
-
-
 
