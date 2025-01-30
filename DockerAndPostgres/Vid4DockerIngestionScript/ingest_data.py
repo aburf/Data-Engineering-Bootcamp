002c36fa832ffbabaaf45b9b6743b105b974766f
Andrew <77085812+aburf@users.noreply.github.com>
2025-01-30 01:41:51 +0000

    updated python script

--- DockerAndPostgres/Vid4DockerIngestionScript/ingest_data.py
+++ DockerAndPostgres/Vid4DockerIngestionScript/ingest_data.py
@@ -16,43 +16,53 @@ def main(params):
         port = params.port
         db = params.db
         table_name = params.tabe_name
-        data_url = '/workspaces/Data-Engineering-Bootcamp/yellow_tripdata_1000.csv'
+        data_url = params.url ##'/workspaces/Data-Engineering-Bootcamp/yellow_tripdata_1000.csv'
         csv_name = data_url.split('/')[-1]
 
     os.system(f"wget {url} -O {csv_name}")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-    df_iter = pd.read_csv(data_url,iterator=True, chunksize=100)
+    df_iter = pd.read_csv(csv_name,iterator=True, chunksize=100)
     
     df = next(df_iter)
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists = 'replace')
+    df.to_sql(name=table_name, con=engine, if_exists='append')
     
-    while True:
-        t_start = time()
-        df = next(df_iter)
+    while True: 
 
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        try:
+            t_start = time()
+            
+            df = next(df_iter)
 
-        df.to_sql(name=table_name, con=engine, if_exists = 'append')
-        t_end = time()
-        print('Inserted another chunk..., to %.3f seconds'% (t_end-t_start)) 
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+            df.to_sql(name=table_name, con=engine, if_exists='append')
+
+            t_end = time()
+
+            print('inserted another chunk, took %.3f second' % (t_end - t_start))
+
+        except StopIteration:
+            print("Finished ingesting data into the postgres database")
+            break
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
                         description='Ingest csv data to postgres',
                         epilog='bottomo text')
-    parser.add_argument('user')
-    parser.add_argument('pass')
-    parser.add_argument('host')
-    parser.add_argument('port')
-    parser.add_argument('db')
-    parser.add_argument('table_name')
-    parser.add_argument('url')
+    parser.add_argument('--user')
+    parser.add_argument('--password')
+    parser.add_argument('--host')
+    parser.add_argument('--port')
+    parser.add_argument('--db')
+    parser.add_argument('--table_name')
+    parser.add_argument('--url')
 
     args = parser.parse_args()
 
